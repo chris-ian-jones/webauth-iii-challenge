1d78b5742438c2782c3a60a60592bf048eba2e97
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-09-18 22:01:04 +0100

    Completed MVP: tidied up code

--- config/restricted-middleare.js
+++ config/restricted-middleare.js
@@ -8,13 +8,13 @@ module.exports = (req, res, next) => {
   if (token) {
     jwt.verify(token, secrets.jwtSecret, (error, decodedToken) => {
       if(error) {
-        res.status(401).json({ message: 'You shall not pass' })
+        res.status(401).json({ message: 'You shall not pass!' })
       } else {
         req.user = { username: decodedToken.username }
         next()
       }
     })
   } else {
-    res.status(400).json({ message: 'No credentials provided' })
+    res.status(400).json({ message: 'You shall not pass!' })
   }
 }
